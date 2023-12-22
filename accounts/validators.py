87c3d1d45ef85249a18a978620194bf8d538e0d6
Michael Zalewski <michaelzalewski06@gmail.com>
2023-12-22 08:05:55 -0500

    Food Item CRUD Functionalities

--- accounts/validators.py
+++ accounts/validators.py
@@ -4,7 +4,6 @@ import os
 
 def allow_only_images_validator( value ):
   ext = os.path.splitext( value.name )[ 1 ]
-  print( ext )
-  valid_extensions = [ 'jpg', 'png', 'jpeg' ]
+  valid_extensions = [ '.jpg', '.png', '.jpeg' ]
   if not ext.lower() in valid_extensions:
     raise ValidationError( f'Unsupported file extension. Allowed extensions: {valid_extensions}' )
--- vendor/views.py
+++ vendor/views.py
@@ -11,7 +11,7 @@ from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
 from accounts.views import check_role_vendor
 
-from menu.forms import CategoryForm
+from menu.forms import CategoryForm, FoodItemForm
 from menu.models import Category, FoodItem
 
 def get_vendor( request ):
@@ -69,6 +69,8 @@ def fooditems_by_category( request, pk=None ):
   }
   return render( request, 'vendor/fooditems_by_category.html', context )
 
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
 def add_category( request ):
   if request.method == 'POST':
     form = CategoryForm( request.POST )
@@ -87,6 +89,8 @@ def add_category( request ):
   }
   return render( request, 'vendor/add_category.html', context )
 
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
 def edit_category( request, pk=None ):
   category = get_object_or_404( Category, pk=pk )
   if request.method == 'POST':
@@ -107,19 +111,64 @@ def edit_category( request, pk=None ):
   }
   return render( request, 'vendor/edit_category.html', context )
 
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
 def delete_category( request, pk=None ):
   category = get_object_or_404( Category, pk=pk )
   category.delete()
   messages.success( request, 'Category has been deleted' )
   return redirect( 'menu_builder' )
 
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
 def add_food( request ):
-  pass
+  if request.method == 'POST':
+    form = FoodItemForm( request.POST, request.FILES )
+    if form.is_valid():
+      food_title = form.cleaned_data[ 'food_title' ]
+      food = form.save( commit=False )
+      food.vendor = get_vendor( request )
+      food.slug = slugify( food_title )
+      form.save()
+      messages.success( request, 'Food Item added successfully' )
+      return redirect( 'fooditems_by_category', food.category.id )
+  else:
+    form = FoodItemForm()
+    form.fields[ 'category' ].queryset = Category.objects.filter( vendor=get_vendor( request ))
+  context = {
+    'form': form,
+  }
+  return render( request, 'vendor/add_food.html', context )
 
-def edit_food( request ):
-  pass
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
+def edit_food( request, pk=None ):
+  food = get_object_or_404( FoodItem, pk=pk )
+  if request.method == 'POST':
+    form = FoodItemForm( request.POST, request.FILES, instance=food )
+    if form.is_valid():
+      food_title = form.cleaned_data[ 'food_title' ]
+      food = form.save( commit=False )
+      food.vendor = get_vendor( request )
+      food.slug = slugify( food_title )
+      form.save()
+      messages.success( request, 'Food Item updated successfully' )
+      return redirect( 'fooditems_by_category', food.category.id )
+  else:
+    form = FoodItemForm( instance=food )
+    form.fields[ 'category' ].queryset = Category.objects.filter( vendor=get_vendor( request ))
+  context = {
+    'food': food,
+    'form': form,
+  }
+  return render( request, 'vendor/edit_food.html', context )
 
-def delete_food( request ):
-  pass
+@login_required( login_url='login' )
+@user_passes_test( check_role_vendor )
+def delete_food( request, pk=None ):
+  food = get_object_or_404( FoodItem, pk=pk )
+  food.delete()
+  messages.success( request, 'Food Item has been deleted' )
+  return redirect( 'fooditems_by_category', food.category.id )
 
 
